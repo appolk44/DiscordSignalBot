98cfcae1979360b7608cc954e1fc4d1791afb67d
appolk44 <110318561+appolk44@users.noreply.github.com>
2022-07-30 22:54:57 -0500

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,5 +1,5 @@
 
 class Secret:
 
-    token = '' # discord bot token
-    signal_channel_id = None # channel id for server the bot will be talking to
+    token = 'MTAwMzEzODE0Nzk5Mjk5Mzg3Mw.GXoKOY.85laEPeop6zWJPZvLBCAiuDUy1wYaOUlB1e-jA' # discord bot token
+    signal_channel_id = 1003145387579814079 # channel id for server the bot will be talking to
